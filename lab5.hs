b09c0d463645ee50fe1b56c1939768f5afa75665
Photoperiod <Wolfsenshi@gmail.com>
2017-10-02 17:04:48 -0700

    sdcqw

--- lab5.hs
+++ lab5.hs
@@ -3,10 +3,15 @@
 -- Mark Philipp
 -- 109941708
 
+-- alphabet. Only inputs the machine can have. Any combination of a and b
 sigma = ['a', 'b']
 
 -- Finite State Machine M = (Q, q0, F, d)
 type FSM = ([Int], Int, [Int], [(Int,Char,Int)])
+
+-- machine that accepts at least one a
+ex1 :: FSM
+ex1 = ([0, 1], 0, [1], [(0, 'a', 0), (0, 'b', 1), (1, 'a', 1), (1, 'b', 1)])
 
 -- Check whether a finite state machine (qs, q0, fs, ts) is correct/complete:
 -- (1) States qs are unique (no duplicates)
@@ -14,8 +19,22 @@ type FSM = ([Int], Int, [Int], [(Int,Char,Int)])
 -- (3) Final states are states (fs is a subset of qs; don't worry about dups)
 -- (4) Transition relation is a function from qs and sigma to qs (exactly one
 --     output state for each state and letter from sigma)
+
+-- test for duplicates
+noDuplicates :: [Int] -> Bool
+noDuplicates [] = True           
+noDuplicates (x:xs) = not (x `elem` xs) && noDuplicates xs
+
+subset :: [Int] -> [Int] -> Bool
+subset [] ys = True
+subset xs [] = True
+subset xs ys = and [a `elem` ys | a <- xs]
+
 checkFSM :: FSM -> Bool
-checkFSM (qs, q0, fs, ts) = undefined
+checkFSM (qs, q0, fs, ts) = noDuplicates qs && q0 `elem` qs && subset fs qs && and[length [(q,a)] == 1 && or[(q, a) == (x, y) | (x, y, z) <- ts] | q <- qs, a <- sigma]
+
+-- && or[(a, b) == (c, d) | (a, b) <- cartesian qs, (c, d, e) <- ts] 
+-- && or[c `elem` qs | (a, b, c) <- ts]
 
 -- Gives the transition function of the machine as a function
 -- i.e., delta m q a = the state machine m goes to when reading a in state q
